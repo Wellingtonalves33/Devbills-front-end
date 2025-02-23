fd6c4078fca6fbe39c2e86e10da1ee20005b2e7e
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-02-23 18:06:06 -0300

    criando o title

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,25 @@
 import { Button } from '../../components/button';
 import { Logo } from '../../components/logo';
-import { Header } from './styles';
+import { Title } from '../../components/title';
+import { Filters, Header, Main, Secyion } from './styles';
 
 export function Home() {
   return (
-    <Header>
-      <Logo />
-      <div>
-        <Button variant="outline">Nova transação</Button>
-        <Button variant="outline">Nova categoria</Button>
-      </div>
-    </Header>
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button variant="outline">Nova transação</Button>
+          <Button variant="outline">Nova categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Secyion>
+          <Filters>
+            <Title title="Saldo" subtitle="Receita e despesas no periodo" />
+          </Filters>
+        </Secyion>
+      </Main>
+    </>
   );
 }
