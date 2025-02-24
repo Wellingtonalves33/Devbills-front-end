2b4f8745b3579ee483b6c75fa6dba91f944529bc
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-02-23 21:09:50 -0300

    criei duas tabelas para ter grafico de gasto e evplução finaceira

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,7 +5,17 @@ import { Card } from '../../components/card';
 import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
-import { Balance, Filters, Header, InputGroup, Main, Secyion } from './styles';
+import {
+  Balance,
+  ChartAction,
+  ChartContainer,
+  ChartContent,
+  Filters,
+  Header,
+  InputGroup,
+  Main,
+  Secyion,
+} from './styles';
 
 export function Home() {
   return (
@@ -24,18 +34,20 @@ export function Home() {
             <InputGroup>
               <InputMask
                 component={Input}
-                mask="dd/mm/yyyy"
-                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                 variant="dark"
                 label="Inicio"
+                placeholder="dd/mm/aaaa"
               />
 
               <InputMask
                 component={Input}
-                mask="dd/mm/yyyy"
-                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                 variant="dark"
                 label="Fim"
+                placeholder="dd/mm/aaaa"
               />
               <ButtonIcon />
             </InputGroup>
@@ -45,6 +57,36 @@ export function Home() {
             <Card title="Saldo" amount={1000000} vartiant="incomes" />
             <Card title="Saldo" amount={1000000} vartiant="expenses" />
           </Balance>
+          <ChartContainer>
+            <header>
+              <Title
+                title="Gastos"
+                subtitle="Despesas por categoria no periodo"
+              />
+            </header>
+            <ChartContent> </ChartContent>
+          </ChartContainer>
+
+          <ChartContainer>
+            <header>
+              <Title
+                title="Evolução Financeira"
+                subtitle="Despesas por categoria no periodo"
+              />
+              <ChartAction>
+                <InputMask
+                  component={Input}
+                  mask="aaaa"
+                  replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
+                  variant="black"
+                  label="Ano"
+                  placeholder="aaaa"
+                />
+                <ButtonIcon />
+              </ChartAction>
+            </header>
+            <ChartContent> </ChartContent>
+          </ChartContainer>
         </Secyion>
       </Main>
     </>
